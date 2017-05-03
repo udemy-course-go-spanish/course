2253e97e9dcddb30929d4bcafdb886bd7a00b3b3
Alexys Lozada <alexyslc@gmail.com>
2017-05-02 23:29:32 -0500

    03 Estructura de un archivo go

---
+++ 03-03-estructura/main.go
@@ -0,0 +1,31 @@
+/*
+este es un comentario
+de bloque
+func main() {
+	fmt.Println("hola mundo")
+}
+*/
+
+// Este es un comentario en línea.
+// Documentar nuestras aplicaciones
+package main
+
+import (
+	"fmt"
+)
+
+/*
+var (
+	saludo string
+	edad int
+)
+
+const (
+	numero = 8
+	nombre = "Leidy"
+)
+*/
+
+func main() {
+	fmt.Println("Hola mundo")
+}
