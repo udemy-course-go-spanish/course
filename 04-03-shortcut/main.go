4044206494a8fb54b6367ce51b50554fa907dfee
Alexys Lozada <alexyslc@gmail.com>
2017-05-16 22:08:55 -0500

    Shortcut Reglas

--- 04-03-shortcut/main.go
+++ 04-03-shortcut/main.go
@@ -9,5 +9,7 @@ func main() {
 	// nombre, apellido, edad := "Alexys", "Lozada", 37
 	a, b := 5, 10
 	a, b = b, a
-	fmt.Println(a, b)
+	// tenemos más líneas de código
+	c, a := 3, 4
+	fmt.Println(a, b, c)
 }
